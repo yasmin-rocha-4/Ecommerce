66dac3bf4ab94739de53bfd39c0bbc32c5445d5b
yasmin-rocha-4 <yasminsrmd@gmail.com>
2025-01-31 08:31:41 -0300

    all products sem filtro

--- ecommerce/src/AllProducts.tsx
+++ ecommerce/src/AllProducts.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import CardProduto from "./CardProduto";
 import UseProdutos from "./Produtos";
-
+import NavBarCarrinho from "./NavBarCarrinho";
+import iconeFiltro from "./assets/Icon/sliders.svg"
 const AllProducts: React.FC = () => {
   const { produtos, loading, error } = UseProdutos(); // Desestruturação do hook
 
@@ -15,8 +16,12 @@ const AllProducts: React.FC = () => {
 
   return (
     <div style={{fontFamily:"Montserrat, serif"}}>
-      
+      <NavBarCarrinho/>
       <p style={{fontSize:"1.5rem", fontWeight:"bold", margin:"20px"}}>All Products</p>
+      <button style={{width:"90%", height:"40px",display:"flex", gap:"1rem", justifyContent:"center", alignItems:"center", margin:"20px", borderRadius:"8px", borderColor: "#bababa", background:"#FFFF", marginBottom:"100px"}}>
+        <img src={iconeFiltro} alt="" />
+        <p style={{margin:"0"}}>filter</p>
+      </button>
       <CardProduto ListaProduto={produtos} /> 
     </div>
   );
