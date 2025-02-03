7f14ee6d7f90b8eaaf1bb1112578b7e36e5c0833
yasmin-rocha-4 <yasminsrmd@gmail.com>
2025-02-03 13:25:44 -0300

    tirando bootstrap

--- ecommerce/vite.config.ts
+++ ecommerce/vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Adiciona alias para o Bootstrap
-      'bootstrap': 'node_modules/bootstrap/dist/css/bootstrap.min.css',
-    },
-  },
 })
